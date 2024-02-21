3b44c1e6df116443dc24b75b1f8fbd8dbb2deeb3
Serhii Mykhailov <SMonikat03@gmail.com>
2024-02-21 12:03:46 +0200

    Add project files.

---
+++ src/AuctionApp.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AuctionApp.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/AuctionApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AuctionApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
